414f3e608d0a00d4492875cf0ecc2c4e41852698
vitaly <vitaly.nevgen@gmail.com>
2018-06-27 21:31:36 +0300

    done with csv reader/writer

--- lesson5/info.md
+++ lesson5/info.md
@@ -1,3 +1,5 @@
 CSV Module in Python:
 https://docs.python.org/3.6/library/csv.html
 
+Python built-in open function:
+https://docs.python.org/3/library/functions.html#open
--- lesson5/write_csv.py
+++ lesson5/write_csv.py
@@ -2,26 +2,24 @@ import csv
 import random
 
 
-def get_coordinates_list(count):
-    result = []
+def coordinates_generator(count):
     for i in range(count):
         X = random.randint(0, 100)
         Y = random.randint(0, 100)
         Z = random.randint(0, 100)
+        yield {
+            'X Cord': X,
+            'Y Cord': Y,
+            'Z Cord': Z,
+        }
 
-        result.append((X, Y, Z))
 
-    return result
+f = open('generated.csv', 'w')
 
+fieldnames = ['X Cord', 'Y Cord', 'Z Cord']
+writer = csv.DictWriter(f, fieldnames=fieldnames)
 
-def get_coordinates_generator(count):
-    for i in range(count):
-        X = random.randint(0, 100)
-        Y = random.randint(0, 100)
-        Z = random.randint(0, 100)
+for cords in coordinates_generator(1):
+    writer.writerow(cords)
 
-        yield (X, Y, Z)
-
-for cords in get_coordinates_list(5):
-    print(cords)
 
