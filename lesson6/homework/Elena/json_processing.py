ac7b74fc1c3aabf4c4b41002cd5b9a40da07a97f
elenaBuko <elena.buko@mail.ru>
2018-07-05 18:08:13 +0300

    Homework: Correction according to the comments + pretty xml structure

--- lesson6/homework/Elena/json_processing.py
+++ lesson6/homework/Elena/json_processing.py
@@ -4,9 +4,21 @@ f = open('quiz_json.json')
 quiz = json.load(f)
 print("Before file changes: ", quiz)
 
-feed = {"results": {"sport": {"q1": {"result1": True, "result2": False, "result3": True}},
-        "math": {"q1": {"result1": False}, "q2": {"result1": False}}}}
-
+feed = {
+	"results": {
+		"sport": {
+			"q1": {
+				"result1": True,
+				"result2": False,
+				"result3": True
+			}
+		},
+		"math": {
+			"q1": {"result1": False},
+			"q2": {"result1": False}
+		}
+	}
+}
 quiz.update(feed)
 
 with open('quiz_json.json', 'w') as file:
