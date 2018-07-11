fa50d4bb75f60948c95e60def57effbebf9217b3
Julia <antonovich.jl@gmail.com>
2018-07-11 14:40:58 +0300

    All tasks done, but file process_images.py was coped from previous lesson

---
+++ lesson8/hometasks/Julia_HW/process_images.py
@@ -0,0 +1,41 @@
+import json
+import os.path
+from PIL import Image, ImageOps
+
+
+def get_jobs():
+    jobs_file = 'jobs.json'
+    f = open(jobs_file, 'r')
+    jobs = json.load(f)
+    return jobs
+
+
+def resize_image(infile, outfile, size, preview_type):
+    size_tuple = (size, size)
+    im = Image.open(infile)
+
+    if preview_type == 'thumbnail':
+        im.thumbnail(size_tuple)
+    elif preview_type == 'fit':
+        im = ImageOps.fit(im, size_tuple, Image.ANTIALIAS)
+
+    im.save(outfile, "JPEG")
+
+
+if __name__ == '__main__':
+    jobs = get_jobs()
+
+    for job in jobs:
+        filename = job['filename']
+
+        for preview in job['previews']:
+            name, ext = os.path.splitext(filename)
+
+            preview_filename = '{name}_{type}{size}{ext}'.format(
+                name=name,
+                type=preview['type'],
+                size=preview['size'],
+                ext=ext
+            )
+
+            resize_image(filename, preview_filename, preview['size'], preview['type'])
