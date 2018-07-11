c8af5eb0de17408af93133625ebc0839be34535d
vitaly <vitaly.nevgen@gmail.com>
2018-07-11 19:25:33 +0300

    WIP lesson 8

---
+++ lesson8/users.py
@@ -0,0 +1,9 @@
+users = [
+    {'name': 'Jack', 'gender': 'm', 'age': 20, 'city': 'Minsk'},
+    {'name': 'Ann', 'gender': 'f', 'age': 36, 'city': 'Minsk'},
+    {'name': 'Piotr', 'gender': 'm', 'age': 27, 'city': 'Minsk'},
+    {'name': 'Masha', 'gender': 'f', 'age': 88, 'city': 'New York'},
+    {'name': 'Andrew', 'gender': 'm', 'age': 42, 'city': 'Bryansk'},
+]
+
+
